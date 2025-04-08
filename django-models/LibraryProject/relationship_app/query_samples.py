d0c1b6d9dac5914a5926766316001cb9b61206d0
eslafat <109255696+eslafat@users.noreply.github.com>
2025-04-08 10:55:19 +0200

    Add files via upload

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,23 @@
+from relationship_app.models import Author, Book
+from relationship_app.models import Library
+from relationship_app.models import Library, Librarian
+
+author_name = "eslam"
+
+author = Author.objects.get(name=author_name)
+books = Book.objects.filter(author=author)
+print(f"Books by {author.name}:")
+for book in books:
+    print(f"- {book.title}")
+    
+    
+
+library = Library.objects.get(name="lib5")
+books = library.books.all()
+print(f"Books in {library.name}:")
+for book in books:
+    print(f"- {book.title}")
+    
+library_name = "lib5"
+librarian = Librarian.objects.get(library = library_name)
+print(f"{librarian} who is working in {library_name}")
