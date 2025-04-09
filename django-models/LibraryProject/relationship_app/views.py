05b11eca78041d05f78352b175a2ed77dd194227
eslafat <109255696+eslafat@users.noreply.github.com>
2025-04-09 03:24:28 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Book
 from .models import Library
 from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
-from django.contrib.auth import login
+from django.views import View
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 
@@ -17,3 +20,16 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
+class UserRegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'relationship_app/register.html', {'form': form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        return render(request, 'relationship_app/register.html', {'form': form})
